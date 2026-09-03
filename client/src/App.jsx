b69d505fec67aaf8a5c17942b7e81e1add3cb7b4
Andrii_22 <a_ishchuk22@ukr.net>
2026-09-03 13:34:53 +0300

    Set up client (Vite+React+Tailwind+MUI) and server deps, add ESLint+Prettier config

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-blue-600">Wordly</h1>;
+      <Button variant="contained">Test</Button>
+    </>
+  );
+}
+
+export default App;
